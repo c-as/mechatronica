be80436a4869c8ef7f61c91e85e05ff3b85e604b
Cas Brugman <casbrugman@hotmail.nl>
2021-10-07 20:46:35 +0200

    nog meer opmaak

--- main.c
+++ main.c
@@ -4,45 +4,45 @@
 
 int main(void)
 {
-        ZetPinDirectories();
+    ZetPinDirectories();
 
-        //brug is dicht doorvaart verboden
-        DoorvaartGeslotenBrugMetTegenliggers();
+    //brug is dicht doorvaart verboden
+    DoorvaartGeslotenBrugMetTegenliggers();
 
-        //sensor ziet boot
-        //Meld mensen op de brug met zoemer
-        ZOEMERAAN;
+    //sensor ziet boot
+    //Meld mensen op de brug met zoemer
+    ZOEMERAAN;
 
-        //als brug vrij is:
-        //Zoemer uit
-        ZOEMERUIT;
+    //als brug vrij is:
+    //Zoemer uit
+    ZOEMERUIT;
 
-        //brug open & lampen brug is bijna open
-        DoortvaartBijnaToegestaanLeds();
+    //brug open & lampen brug is bijna open
+    DoortvaartBijnaToegestaanLeds();
 
-        //als brug open is:
-        //lampen voor boot doorvaart:
-        DoorvaartToegestaanLeds();
+    //als brug open is:
+    //lampen voor boot doorvaart:
+    DoorvaartToegestaanLeds();
 
-        //Sensor ziet dat de boot voorbij is:
-        //lampen gestremde doorvaart
-        DoorvaartGenaderdenLeds();
+    //Sensor ziet dat de boot voorbij is:
+    //lampen gestremde doorvaart
+    DoorvaartGenaderdenLeds();
 
-        //wacht 5 seconden
-        //brug dicht & lampen verboden doorvaart
-        DoorvaartGeslotenBrugMetTegenliggers();
+    //wacht 5 seconden
+    //brug dicht & lampen verboden doorvaart
+    DoorvaartGeslotenBrugMetTegenliggers();
 
-    while(1)
+    while (1)
     {
-        while(groeneLedsKnipperen)
-    {
-        GROENELED1AAN;
-        GROENELED2AAN;
-        _delay_ms(1000);
-        GROENELED1UIT;
-        GROENELED2UIT;
-        _delay_ms(1000);
-    }
+        while (groeneLedsKnipperen)
+        {
+            GROENELED1AAN;
+            GROENELED2AAN;
+            _delay_ms(1000);
+            GROENELED1UIT;
+            GROENELED2UIT;
+            _delay_ms(1000);
+        }
     }
 
     return 0;
