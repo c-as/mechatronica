0a68b6088ed19217eedc4a50e87b87e64229eab6
axa112 <axeldbmc@gmail.com>
2021-10-15 09:33:05 +0200

    Niet meer nodig

--- main.c
+++ main.c
@@ -38,44 +38,6 @@ void start_brug()
 
 int main(void)
 {
-    //brug is dicht doorvaart verboden
-//    DoorvaartGeslotenBrugMetTegenliggers();
-
-    //sensor ziet boot
-    //Meld mensen op de brug met zoemer
-//    ZOEMERAAN;
-
-    //als brug vrij is:
-    //Zoemer uit
-//    ZOEMERUIT;
-
-    //brug open & lampen brug is bijna open
-//    DoortvaartBijnaToegestaanLeds();
-
-    //als brug open is:
-    //lampen voor boot doorvaart:
-//    DoorvaartToegestaanLeds();
-
-    //Sensor ziet dat de boot voorbij is:
-    //lampen gestremde doorvaart
-//    DoorvaartGenaderdenLeds();
-
-    //wacht 5 seconden
-    //brug dicht & lampen verboden doorvaart
-//    DoorvaartGeslotenBrugMetTegenliggers();
-
-//    while (1)
-//    {
-//        while (groeneLedsKnipperen)
-//        {
-//            GROENELED1AAN;
-//            GROENELED2AAN;
-//            _delay_ms(1000);
-//            GROENELED1UIT;
-//            GROENELED2UIT;
-//            _delay_ms(1000);
-//        }
-//    }
 
     start_brug();
 
